67aaa7ddd97407dae16fa72da60a79400b06243a
SouravO <o.sourav5@gmail.com>
2026-02-05 11:33:54 +0530

    cursor

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import RocketCursor from './components/RocketCursor';
 import Home from './pages/Home';
 import About from './pages/About';
 import Courses from './pages/Courses';
@@ -24,6 +25,7 @@ function AppContent() {
 
   return (
     <div className="flex flex-col min-h-screen w-full">
+      <RocketCursor />
       <Navbar show={showNavbar} />
       <main className="flex-1 w-full bg-gray-50">
           <Routes>
